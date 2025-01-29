0c690c54afa7bbc32909a80f78c531e14f8daef8
Michelle Findlay <mf@lp0.uk>
2025-01-29 18:44:11 +0000

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -11,27 +11,21 @@ export default function Page() {
           30 "ish" year old person. Loves to Binge films and tv. All opinions are my own and not that of my employers et cetera et cetera.`}
       </p>
       <p className="mb-4">
-        {`Chief Harlequin at `}
-      </p><a
-            className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
-            rel="noopener noreferrer"
-            target="_blank"
-            href="https://www.jesterscourt.uk"
-          >
-            <ArrowIcon />
-            <p className="ml-2 h-7">Jesters Court</p>
-          </a>
+        {`Chief Harlequin at Jesters Court`}
+      </p>
 
       <p className="mb-4">
-        {`Trustee at `}
-      </p><a
+        {`Trustee at Pride in Gloucestershire`}
+      </p>
+
+          <a
             className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
             rel="noopener noreferrer"
             target="_blank"
-            href="https://www.prideinglos.org.uk"
+            href="https://github.com/MichelleFindlay/lp0.uk"
           >
             <ArrowIcon />
-            <p className="ml-2 h-7">🏳️‍🌈 Pride in Gloucestershire 🏳️‍⚧️</p>
+            <p className="ml-2 h-7">view source</p>
           </a>
       
       <div className="my-8">
