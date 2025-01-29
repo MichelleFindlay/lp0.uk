5d6d04b3aa6700e5dfafee080b555c526a15b3bd
Michelle Findlay <mf@lp0.uk>
2025-01-29 18:01:48 +0000

    Update footer.tsx

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -35,7 +35,29 @@ export default function Footer() {
             className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
             rel="noopener noreferrer"
             target="_blank"
-            href="https://github.com/vercel/next.js"
+            href="https://bsky.app/profile/lp0.uk"
+          >
+            <ArrowIcon />
+            <p className="ml-2 h-7">bluesky</p>
+          </a>
+        </li>
+        <li>
+          <a
+            className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
+            rel="noopener noreferrer"
+            target="_blank"
+            href="https://mastodon.online/@lp0"
+          >
+            <ArrowIcon />
+            <p className="ml-2 h-7">mastodon</p>
+          </a>
+        </li>
+        <li>
+          <a
+            className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
+            rel="noopener noreferrer"
+            target="_blank"
+            href="https://github.com/MichelleFindlay"
           >
             <ArrowIcon />
             <p className="ml-2 h-7">github</p>
@@ -46,7 +68,7 @@ export default function Footer() {
             className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
             rel="noopener noreferrer"
             target="_blank"
-            href="https://vercel.com/templates/next.js/portfolio-starter-kit"
+            href="https://github.com/MichelleFindlay/lp0.uk"
           >
             <ArrowIcon />
             <p className="ml-2 h-7">view source</p>
@@ -54,7 +76,7 @@ export default function Footer() {
         </li>
       </ul>
       <p className="mt-8 text-neutral-600 dark:text-neutral-300">
-        © {new Date().getFullYear()} MIT Licensed
+        © {new Date().getFullYear()} LP0.uk
       </p>
     </footer>
   )
