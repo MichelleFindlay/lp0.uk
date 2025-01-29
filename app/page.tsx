2d6a5eb9e11b5c1b14d3e87cd454150846351897
Michelle Findlay <mf@lp0.uk>
2025-01-29 18:46:44 +0000

    Update page.tsx
    This will do for now i give up on adding hyperlinks to this bit..

--- app/page.tsx
+++ app/page.tsx
@@ -10,13 +10,6 @@ export default function Page() {
         {`🏳️‍⚧️⚤🏳️‍🌈🏴‍☠️
           30 "ish" year old person. Loves to Binge films and tv. All opinions are my own and not that of my employers et cetera et cetera.`}
       </p>
-      <p className="mb-4">
-        {`Chief Harlequin at Jesters Court`}
-      </p>
-
-      <p className="mb-4">
-        {`Trustee at Pride in Gloucestershire`}
-      </p>
       
       <div className="my-8">
         <BlogPosts />
