690143e05d5f0af9ce13962e470c8dadc5c68dd9
Michelle Findlay <mf@lp0.uk>
2025-01-29 18:15:29 +0000

    Update not-found.tsx
    Custom 404 page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,9 +2,9 @@ export default function NotFound() {
   return (
     <section>
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        404 - Page Not Found
+        404 - Yellow Bricks Not Found
       </h1>
-      <p className="mb-4">The page you are looking for does not exist.</p>
+      <p className="mb-4">What are you doing here? How did you even get here? This isn't the yellow brick road!</p>
     </section>
   )
 }
