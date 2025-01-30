dbd5ec45e29419550ad1db653c83c936242359d8
Michelle Findlay <mf@lp0.uk>
2025-01-30 15:22:27 +0000

    Create page.tsx
    Addition of a contact sub URL

---
+++ app/contact/page.tsx
@@ -0,0 +1,19 @@
+import { BlogPosts } from 'app/components/posts'
+
+export default function Page() {
+  return (
+    <section>
+      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
+        Hey I'm Michelle
+      </h1>
+      <p className="mb-4">
+        {`🏳️‍⚧️⚤🏳️‍🌈🏴‍☠️
+          30 "ish" year old person. Loves to Binge films and tv. All opinions are my own and not that of my employers et cetera et cetera.`}
+      </p>
+      
+      <div className="my-8">
+        <BlogPosts />
+      </div>
+    </section>
+  )
+}
