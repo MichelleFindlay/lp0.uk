be50e82ec5165f8666e82f3937be197ef59abfba
Michelle Findlay <mf@lp0.uk>
2025-09-16 19:46:53 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -7,8 +7,7 @@ export default function Page() {
         Hey I'm Michelle
       </h1>
       <p className="mb-4">
-        {`🏳️‍⚧️⚤🏳️‍🌈🏴‍☠️
-          30 "ish" year old person. Loves to Binge films and tv. All opinions are my own and not that of my employers et cetera et cetera.`}
+        {`👩 30 "ish" year old woman. Loves to Binge films and tv. All opinions are my own and not that of my employers et cetera et cetera.`}
       </p>
       
       <div className="my-8">
