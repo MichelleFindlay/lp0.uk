25381034f4520984e3d0b7b373723d2904dddc59
Michelle Findlay <mf@lp0.uk>
2025-09-16 19:35:18 +0100

    Update footer.tsx

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -35,28 +35,6 @@ export default function Footer() {
             className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
             rel="noopener noreferrer"
             target="_blank"
-            href="https://bsky.app/profile/lp0.uk"
-          >
-            <ArrowIcon />
-            <p className="ml-2 h-7">bluesky</p>
-          </a>
-        </li>
-        <li>
-          <a
-            className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
-            rel="noopener noreferrer"
-            target="_blank"
-            href="https://mastodon.online/@lp0"
-          >
-            <ArrowIcon />
-            <p className="ml-2 h-7">mastodon</p>
-          </a>
-        </li>
-        <li>
-          <a
-            className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
-            rel="noopener noreferrer"
-            target="_blank"
             href="https://github.com/MichelleFindlay"
           >
             <ArrowIcon />
